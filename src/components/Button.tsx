14c6fb75b17ff51f9320f5865a2532b33e586015
shpokas <s.shpokas@gmail.com>
2025-02-18 12:27:58 +0200

    fix: interface -> type

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
-import React, { ButtonHTMLAttributes } from "react";
+import React from "react";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button: React.FC<ButtonProps> = ({ children, ...props }) => (
   <button
