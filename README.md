4cb9c3630832afe60f64fb1ef06ae788f570fe07
shpokas <s.shpokas@gmail.com>
2025-02-18 10:27:48 +0200

    Initial setup

---
+++ README.md
@@ -0,0 +1,10 @@
+# React + TypeScript + Vite
+
+**This is a template, feel free to use**
+
+# Versions
+
+- React 19.0.0
+- Tailwind 4.0.6
+- TypeScript 5.7.2
+- Vite 6.1.0
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <button className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow-lg hover:bg-blue-600">
+        Tailwind Button
+      </button>
+    </div>
+  );
+};
+
+export default App;
